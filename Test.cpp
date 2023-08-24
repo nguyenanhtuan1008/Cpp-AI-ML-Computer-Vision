ddd2f60f294ee4c08f3e0053265f07181f54a452
nguyenanhtuan1008 <nguyenanhtuan1008@gmail.com>
2023-08-24 17:52:20 +0900

    Test camera ok

--- Test.cpp
+++ Test.cpp
@@ -7,9 +7,32 @@ using namespace cv;
 using namespace std;
 
 //////////// Images ////////////
+//void main() {
+//	string path = "Resources/test.png";
+//	Mat img = imread(path);
+//	imshow("Image", img);
+//	waitKey(0);
+//}
+
+//////// Video ////////////
+//void main() {
+//	string path = "Resources/test_video.mp4";
+//	VideoCapture cap(path);
+//	Mat img;
+//	while (true) {
+//		cap.read(img);
+//		imshow("Image", img);
+//		waitKey(20);
+//	}
+//}
+
+////// camera /////
 void main() {
-	string path = "Resources/test.png";
-	Mat img = imread(path);
-	imshow("Image", img);
-	waitKey(0);
+	VideoCapture cap(0);
+	Mat img;
+	while (true) {
+		cap.read(img);
+		imshow("Image", img);
+		waitKey(1);
+	}
 }
